3033a87a06a72dba0076b60ac9ac682b72d1c848
Benjamin <benjamin.alazet@gmail.com>
2023-09-19 11:15:10 +0200

    Script that computes the number of tasks completed by user id

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const todoDict = {};
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const todosList = JSON.parse(body);
+
+    for (const todo of todosList) {
+      const userIds = todo.userId;
+      const todoComplete = todo.completed;
+
+      if (!todoDict[userIds]) {
+        todoDict[userIds] = 0;
+      }
+
+      if (todoComplete === true) {
+        todoDict[userIds]++;
+      }
+    }
+    console.log(todoDict);
+  }
+});
