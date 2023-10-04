9b7211519dad701873411c7fa126dc36caffbc4f
Benjamin <benjamin.alazet@gmail.com>
2023-10-04 14:51:00 +0200

    TRy to use readline

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,20 @@
-process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?');
+const readline = require('readline');
 
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  // Show user name
-  console.log(`Your name is: ${name}`);
-  // closing message
-  console.log('This important software is now closing');
-  process.exit(0);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log("Welcome to Holberton School, what is your name?");
+
+rl.on('line', (input) => {
+    const name = input.trim();
+    console.log(`Your name is: ${name}`);
+    rl.close(); // Ferme l'interface readline
+});
+
+// Événement lorsque l'interface readline est fermée
+rl.on('close', () => {
+    console.log("This important software is now closing");
+    process.exit(0);
 });
