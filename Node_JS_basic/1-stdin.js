1b8924543225cc639f18a7cae824ae60899e6e30
Benjamin <benjamin.alazet@gmail.com>
2023-10-05 10:48:43 +0200

    add setTimeout

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,10 +2,12 @@ process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  const name = data.trim();
-  // Show user name
-  console.log(`Your name is: ${name}`);
-  // closing message
-  console.log('This important software is now closing');
-  process.exit(0);
+    const name = data.trim();
+    console.log(`Your name is: ${name}`);
+    console.log('This important software is now closing');
+    
+    // Utilisez setTimeout pour laisser le temps au test de capturer l'entrée utilisateur
+    setTimeout(() => {
+        process.exit(0);
+    }, 100);
 });
