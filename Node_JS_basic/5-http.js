87490bb162e8fbf1eb2aee810209794c2013e2e8
Benjamin <benjamin.alazet@gmail.com>
2023-10-06 23:50:33 +0200

    Add 3-read_file_async code in http.createServer()

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const url = require('url'); // Extract path from URL
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -10,17 +10,31 @@ const app = http.createServer((req, res) => {
   const pathName = parsedUrl.pathname; // Pathname in the obj is the path
 
   if (pathName === '/students') {
-    countStudents('database.csv')
-      .then((data) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((error) => {
+    fs.readFile('database.csv', 'utf-8', (err, data) => {
+      if (err) {
         res.statusCode = 500;
         res.setHeader('Content-Type', 'text/plain');
-        res.end(`Internal Server Error: ${error.message}`);
-      });
+        res.end('Cannot load the database');
+      } else {
+        // 3-read_file_async code
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        const allData = lines.map((line) => line.split(','));
+        const studentData = allData.slice(1);
+
+        const csStudents = studentData.filter((student) => student[3] === 'CS');
+        const sweStudents = studentData.filter((student) => student[3] === 'SWE');
+
+        const csStudentsNames = csStudents.map((student) => student[0]);
+        const sweStudentsNames = sweStudents.map((student) => student[0]);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`This is the list of our students
+Number of students: ${studentData.length}
+Number of students in CS: ${csStudents.length}. List: ${csStudentsNames.join(', ')}
+Number of students in SWE: ${sweStudents.length}. List: ${sweStudentsNames.join(', ')}`);
+      }
+    });
   } else if (pathName === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
