5e5f29be32c288dd987865492ffadd822d939c13
Benjamin <benjamin.alazet@gmail.com>
2023-10-06 23:57:52 +0200

    Add Promise version of fs to use await

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,40 +1,37 @@
 const http = require('http');
 const url = require('url'); // Extract path from URL
-const fs = require('fs');
+const fs = require('fs').promises; // Promise version of fs
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   const parsedUrl = url.parse(req.url, true); // Parse URL -> obj with properties
   const pathName = parsedUrl.pathname; // Pathname in the obj is the path
 
   if (pathName === '/students') {
-    fs.readFile('database.csv', 'utf-8', (err, data) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Cannot load the database');
-      } else {
-        // 3-read_file_async code
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const allData = lines.map((line) => line.split(','));
-        const studentData = allData.slice(1);
+    try {
+      const data = await fs.readFile('database.csv', 'utf-8');
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const allData = lines.map((line) => line.split(','));
+      const studentData = allData.slice(1);
 
-        const csStudents = studentData.filter((student) => student[3] === 'CS');
-        const sweStudents = studentData.filter((student) => student[3] === 'SWE');
+      const csStudents = studentData.filter((student) => student[3] === 'CS');
+      const sweStudents = studentData.filter((student) => student[3] === 'SWE');
 
-        const csStudentsNames = csStudents.map((student) => student[0]);
-        const sweStudentsNames = sweStudents.map((student) => student[0]);
+      const csStudentsNames = csStudents.map((student) => student[0]);
+      const sweStudentsNames = sweStudents.map((student) => student[0]);
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`This is the list of our students
-Number of students: ${studentData.length}
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(`This is the list of our students\nNumber of students: ${studentData.length}
 Number of students in CS: ${csStudents.length}. List: ${csStudentsNames.join(', ')}
 Number of students in SWE: ${sweStudents.length}. List: ${sweStudentsNames.join(', ')}`);
-      }
-    });
+    } catch (error) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Cannot load the database');
+    }
   } else if (pathName === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
