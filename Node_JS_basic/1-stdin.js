bf6f2cc621320a69f739da3a7c964efcf2fa6492
Benjamin <benjamin.alazet@gmail.com>
2023-10-06 11:07:49 +0200

    Test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  const name = data.trim();
-  // Show user name
-  console.log(`Your name is: ${name}`);
-  // closing message
+  console.log(`Your name is: ${data.toString().trim()}`);
+});
+
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit(0);
 });
