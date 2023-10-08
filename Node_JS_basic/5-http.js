910eefdf0a6b5179a0b46e0ff98a75f7685861f0
Benjamin <benjamin.alazet@gmail.com>
2023-10-08 18:23:28 +0200

    Modify resolve from 3-read_file_async.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,41 +1,24 @@
 const http = require('http');
-const url = require('url'); // Extract path from URL
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true); // Parse URL -> obj with properties
-  const pathName = parsedUrl.pathname; // Pathname in the obj is the path
-
-  if (pathName === '/students') {
-    fs.readFile('database.csv', 'utf-8', (err, data) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Cannot load the database');
-      } else {
-        // 3-read_file_async code
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const allData = lines.map((line) => line.split(','));
-        const studentData = allData.slice(1);
-
-        const csStudents = studentData.filter((student) => student[3] === 'CS');
-        const sweStudents = studentData.filter((student) => student[3] === 'SWE');
-
-        const csStudentsNames = csStudents.map((student) => student[0]);
-        const sweStudentsNames = sweStudents.map((student) => student[0]);
-
+  if (req.url === '/students') {
+    countStudents('database.csv')
+      .then((data) => {
+        const response = `This is the list of our students\nNumber of students: ${data.totalStudents}\nNumber of students in CS: ${data.csStudentsCount}. List: ${data.csStudentsList.join(', ')}\nNumber of students in SWE: ${data.sweStudentsCount}. List: ${data.sweStudentsList.join(', ')}`;
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end(`This is the list of our students
-Number of students: ${studentData.length}
-Number of students in CS: ${csStudents.length}. List: ${csStudentsNames.join(', ')}
-Number of students in SWE: ${sweStudents.length}. List: ${sweStudentsNames.join(', ')}`);
-      }
-    });
-  } else if (pathName === '/') {
+        res.end(response);
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`Internal Server Error: ${error.message}`);
+      });
+  } else if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
