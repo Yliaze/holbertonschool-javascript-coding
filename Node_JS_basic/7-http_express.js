28877d3cffe528eacef2d2b0bed49db32ebac380
Benjamin <benjamin.alazet@gmail.com>
2023-10-08 19:14:59 +0200

    Eslint for the win

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -15,8 +15,8 @@ app.get('/students', (req, res) => {
       const response = `This is the list of our students\nNumber of students: ${data.totalStudents}\nNumber of students in CS: ${data.csStudentsCount}. List: ${data.csStudentsList.join(', ')}\nNumber of students in SWE: ${data.sweStudentsCount}. List: ${data.sweStudentsList.join(', ')}`;
       res.send(response);
     })
-    .catch((error) => {
-      res.status(500).send(`This is the list of our students\nCannot load the database`);
+    .catch(() => {
+      res.status(500).send('This is the list of our students\nCannot load the database');
     });
 });
 
