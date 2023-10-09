7e2a8354db649add209b377a6b5df53be61729c2
Benjamin <benjamin.alazet@gmail.com>
2023-10-09 11:56:49 +0200

    Write the Students controller

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,58 @@
+const readDatabase = require('../utils');
+
+const databaseFilename = process.argv[2];
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    response.statusCode = 200;
+    readDatabase(databaseFilename)
+      .then((data) => {
+        const fields = Object.keys(data)
+          .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.write('This is the list of our students\n');
+
+        fields.forEach((field) => {
+          const studentsCount = data[field].length;
+          const studentList = data[field].join(', ');
+          response.write(`Number of students in ${field}: ${studentsCount}. List: ${studentList}\n`);
+        });
+
+        response.end();
+      })
+      .catch((error) => {
+        response.statusCode = 500;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end(`Cannot load the database: ${error.message}`);
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      response.statusCode = 500;
+      response.setHeader('Content-Type', 'text/plain');
+      response.end('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(databaseFilename)
+      .then((data) => {
+        const studentsInMajor = data[major];
+
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end(`List: ${studentsInMajor.join(', ')}`);
+      })
+      .catch((error) => {
+        response.statusCode = 500;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end(`Cannot load the database: ${error.message}`);
+      });
+  }
+}
+
+module.exports = StudentsController;
